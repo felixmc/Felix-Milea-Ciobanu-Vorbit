7e41924e9bcda07ecfe8851345d749ea0eff0470
Felix Milea-Ciobanu <felixmilea@gmail.com>
2014-01-12 12:27:59 -0700

    made log syntax more visually appealing

--- code/com/felixmilea/vorbit/utils/Log.scala
+++ code/com/felixmilea/vorbit/utils/Log.scala
@@ -13,6 +13,8 @@ object Log {
   var printLevel = 0
   var fileLevel = 1
   private var logFile: String = null
+  private var alternateTimestampColor = false
+  private val messagePadding = 7
 
   def init(fLevel: Int = 1, pLevel: Int = 0) {
     printLevel = pLevel
@@ -30,19 +32,23 @@ object Log {
     fw.close()
   }
 
-  protected abstract class Logger(val color: String, val label: String, val level: Int) {
+  protected abstract class Logger(val labelColor: String, val messageColor: String, val label: String, val level: Int) {
+    val padding = " " * (7 - label.length)
     final def apply(message: String) {
-      if (printLevel <= level)
-        out.println(s"[$timestamp] $color${Console.BOLD}$label:${Console.RESET}$color $message${Console.RESET}")
+      if (printLevel <= level) {
+        val tColor = if (alternateTimestampColor) s"${Console.WHITE_B}${Console.BLACK}" else Console.WHITE
+        out.println(s"${Console.RESET}$tColor $timestamp ${Console.RESET}$labelColor $padding$label ${Console.RESET}$messageColor $message${Console.RESET}")
+        alternateTimestampColor = !alternateTimestampColor
+      }
       if (fileLevel <= level)
-        writeToFile(s"[$timestamp] $label: $message")
+        writeToFile(s"[$timestamp] $label:$padding $message")
     }
   }
 
-  object Fatal extends Logger(Console.RED, "Fatal", 4)
-  object Error extends Logger(Console.RED, "Error", 3)
-  object Warning extends Logger(Console.YELLOW, "Warning", 2)
-  object Info extends Logger(Console.BLUE, "Info", 1)
-  object Debug extends Logger(Console.BLACK, "Debug", 0)
+  object Fatal extends Logger(Console.RED_B, Console.RED + Console.BOLD, "Fatal", 4)
+  object Error extends Logger(Console.RED_B, Console.RED + Console.BOLD, "Error", 3)
+  object Warning extends Logger(Console.YELLOW_B, Console.YELLOW + Console.BOLD, "Warning", 2)
+  object Info extends Logger(Console.CYAN_B, Console.CYAN + Console.BOLD, "Info", 1)
+  object Debug extends Logger(Console.GREEN_B, Console.GREEN + Console.BOLD, "Debug", 0)
 
 }
