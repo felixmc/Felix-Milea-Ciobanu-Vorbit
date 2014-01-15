2b3060090a9237a0024cddcf61c04d5e6e2dd951
Felix Milea-Ciobanu <felixmilea@gmail.com>
2014-01-15 02:56:29 -0700

    finished miner config class

--- code/com/felixmilea/vorbit/reddit/mining/MinerConfig.scala
+++ code/com/felixmilea/vorbit/reddit/mining/MinerConfig.scala
@@ -3,14 +3,16 @@ package com.felixmilea.vorbit.reddit.mining
 class MinerConfig {
   var subredditMode = MinerConfig.SubredditMode.Blacklist
   var subreddits = List[String]()
-
+  var postMaxAge = 0
+  var commentMaxAge = 0
+  var postMinKarma = 0
+  var commentMinKarma = 0
+  var minGild = 0
 }
 
 object MinerConfig {
-
   object SubredditMode extends Enumeration {
-    type Direction = Value
+    type SubredditMode = Value
     val Whitelist, Blacklist = Value
   }
-
 }
